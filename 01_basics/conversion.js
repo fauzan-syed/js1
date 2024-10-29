5c98756bebeb066652b78c8b68de41aa6363179d
fauzan-syed <fauzansyed360@gmail.com>
2024-10-29 10:33:23 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,16 @@
+let score=null
+let scoree="33abc"
+
+console.log(typeof score)
+console.log(typeof scoree)
+let converttonumber=Number(score)
+let convert=Number(scoree)
+console.log(typeof converttonumber)
+console.log(typeof convert)
+console.log(convert)
+console.log(converttonumber)
+//33=33
+//33abc=nan
+//true=1, false=0
+//null=0
+//emptystring in boolean is false and vice versa
