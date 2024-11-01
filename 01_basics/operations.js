152d014e3493cbca6f591805619067da249fa069
fauzan-syed <fauzansyed360@gmail.com>
2024-11-01 06:11:39 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,26 @@
+// ******************operations************************
+let value=3
+let neg=-value
+console.log(neg)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+let str1="hello"
+let str2=" Fauzan"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+let counterGame=100
+counterGame++
+console.log(counterGame)
+
+
+
+
