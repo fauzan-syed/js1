487015be917d29c5122e6f856e9f800d5785ca40
fauzan-syed <fauzansyed360@gmail.com>
2024-11-01 06:09:45 +0000

    new

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -14,3 +14,32 @@ console.log(converttonumber)
 //true=1, false=0
 //null=0
 //emptystring in boolean is false and vice versa
+
+
+
+// ******************operations************************
+let value=3
+let neg=-value
+console.log(neg)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+let str1="hello"
+let str2=" Fauzan"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+let counterGame=100
+counterGame++
+console.log(counterGame)
+
+
+
+
